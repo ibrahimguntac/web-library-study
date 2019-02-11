0f0919dcc7588898157eed685a643068f0f2c872
İbrahim Güntaç <ibrahimg@cstekno.com.tr>
2019-02-11 14:54:22 +0300

    Update BookController.java
    Deneme

--- src/com/library/springdemo/controller/BookController.java
+++ src/com/library/springdemo/controller/BookController.java
@@ -36,13 +36,10 @@ public class BookController implements InitializingBean{
     @GetMapping("/list")
     public String listBooks(Model theModel) {
 
-        // get the books from service
         List<Book> bookList = bookService.getBooks();
 
-        // add the books to the model
         theModel.addAttribute("books", bookList);
 
-        // return view
         return "book-list";
     }
 
